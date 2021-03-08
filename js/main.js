edf61a3acef9b2a273e367fa32c551d2aca34307
wass01 <maatallawassim01@gmail.com>
2021-03-08 15:58:09 +0100

    ESERCIZIO PAROLA PALINDROMA TERMINATO

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-function parolaPalindroma(){
+var parola = prompt("Dammi una parola:");
+parolaPalindroma(parola);
 
+// function
+function parolaPalindroma(string){
+  var splitParola = string.split("");
+  var reverseParola = splitParola.reverse();
+  var joinParola = reverseParola.join("");
+
+  if (joinParola == parola){
+    console.log("La parola è palindroma");
+  } else {
+    console.log("La parola non è palindroma");
+  }
 }
